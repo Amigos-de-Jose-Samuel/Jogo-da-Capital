208c0fad1482d8e51f05dffda56055a76ee520b8
galileuveloso <galileu.veloso@hotmail.com>
2022-06-24 15:53:01 -0300

    implementation of drawing methods and result validation

--- app/src/main/java/samuel/jose/jogodacapital/MainActivity.java
+++ app/src/main/java/samuel/jose/jogodacapital/MainActivity.java
@@ -8,12 +8,17 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.util.Locale;
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
     String[] states = {"Acre", "Alagoas", "Amapá", "Amazonas", "Bahia", "Ceará", "Espírito Santo", "Goiás", "Maranhão", "Mato Grosso", "Mato Grosso do Sul", "Paraíba", "Piauí", "Roraima", "Rondônia"};
     String[] cities = {"Rio Branco", "Maceió", "Macapá", "Manaus", "Salvador", "Fortaleza", "Vitória", "Goiânia", "São Luís", "Cuiabá", "Campo Grande", "João Pessoa", "Teresina", "Boa Vista", "Porto Velho"};
     TextView score, state, output;
     EditText input;
     Button guessBtn, nextBtn;
+    Random random = new Random();
+    int actual, count;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,16 +33,41 @@ public class MainActivity extends AppCompatActivity {
         nextBtn = findViewById(R.id.nextBtn);
 
         nextBtn.setEnabled(false);
-        
-//        Criar função que compara duas strings, removendo caracteres especiais e ignorando caixa alta
-//        Sortear um número a cada rodada que representará o estado e cidade nos arrays states e cities
+
+        draw(); //sorteia um estado inicial
+        score.setText("0"); //seto o score inicial pra zero
+
+        nextBtn.setEnabled(false);//desabilito o botao de proximo
+
+        count = 1;//inicio o contador de perguntas pra primeira
     }
 
-    public void guess(View view) {
-//        Verificar se acertou ou errou e alterar o output, habilitar botão de próximo e desabilitar botão de enviar
+    public void guess(View view)
+    {
+        String res = (input.getText().toString()).toLowerCase(Locale.ROOT);//pego o valor respondido
+        String capital = cities[actual].toLowerCase(Locale.ROOT);//pego a resposta correta
+
+        if(res.equals(capital)) {
+            output.setText("Correto!");
+            int scoreAtual = Integer.parseInt(score.getText().toString()); // aumento o score
+            score.setText(scoreAtual++);
+        } else
+            output.setText("Erro, resposta correta: " + capital);
+
+        nextBtn.setEnabled(count < 5); // se for a quinta resposta ele termina o jogo e nao deixa dar next
+        guessBtn.setEnabled(false);
     }
 
     public void next(View view) {
-//        Pŕoximo estado, limpar input, desabilitar botão de próximo
+        draw();
+        output.setText("");
+        nextBtn.setEnabled(false);
+        guessBtn.setEnabled(true);
+        count++;
+    }
+
+    private void draw() {
+        actual = random.nextInt(14);
+        state.setText(states[actual]);
     }
 }
